5911f14f6230b373e61876b785d76c5b38bfe73e
doguhannilt <doguhannilt@gmail.com>
2025-05-09 12:39:11 +0300

    feat(appointment-service): RestTemplateConfig and RestTemplateAddresses added. patient_service and doctor_service urls added.

---
+++ appointment-service/src/main/java/com/project/config/RestTemplateAddresses.java
@@ -0,0 +1,6 @@
+package com.project.config;
+
+public class RestTemplateAddresses {
+    private final static String PATIENT_SERVICE_URL = "http://localhost:4004/api/patients/";
+    private final static String DOCTOR_SERVICE_URL = "http://localhost:4004/api/doctors/";
+}
